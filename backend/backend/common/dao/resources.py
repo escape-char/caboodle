022a07715a9643945390751a60db63208bfbf0de
jordan goss <jordan.m.goss@gmail.com>
2021-06-15 11:10:06 -0400

    Resolves #22, GET my bookmarks
    resolves #22,
    
    implement GET my bookmarks with query parameters

--- backend/backend/common/dao/resources.py
+++ backend/backend/common/dao/resources.py
@@ -8,29 +8,23 @@ from backend.common import schema
 def get_resources(db, offset=0, limit=100, search=""):
     s: Final[str] = f"%{search}%".lower()
     try:
+        query = db.query(models.Resource)
         if search:
-            resources: List[models.Resource] = db.query(
-                models.Resource
-            ).filter(
+            query.filter(
                 or_(
                     models.resource.name.like(s),
                     models.Resource.description.like(s),
                 )
-            ).offset(offset).limit(limit).all()
-
-            return schema.DatabaseResult(
-                success=True,
-                data=resources
             )
-        else:
-            resources = db.query(
-                models.Resource
-            ).offset(offset).limit(limit).all()
 
-            return schema.DatabaseResult(
-                success=True,
-                data=resources
-            )
+        resources: List[models.Resource] = query.offset(
+            offset
+        ).limit(limit).all()
+
+        return schema.DatabaseResult(
+            success=True,
+            data=resources
+        )
     except exc.SQLAlchemyError as e:
         return schema.DatabaseResult(
             success=False,
